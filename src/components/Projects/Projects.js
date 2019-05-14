fe0345ed39dca3ad673041ae4e70baffea671b68
Ciprian Bolboros <cbolboros@gmail.com>
2019-05-14 13:56:55 +0300

    Added the template for a project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import ProjectSuperhero from "./Project-Superhero/ProjectSuperhero";
 import { ReactComponent as Close } from "../../images/close.svg";
+import Button from "@material-ui/core/Button";
+
 import "./Projects.css";
 
 class Projects extends Component {
@@ -10,10 +12,26 @@ class Projects extends Component {
       close: false
     };
   }
-  render() {
+  render(props) {
     const { showMe } = this.props;
     return (
       <div className={"page Projects " + (showMe ? "front-page" : "")}>
+        <div className="project">
+          <div className="project-image" />
+          <div className="project-description">
+            <p className="project-title">Superheroes</p>
+            <p className="project-info">A simple API that get's information about searched hero(s).</p>
+            <Button className="project-show" size="small" variant="contained" color="primary">
+              Try
+            </Button>
+            <ul className="project-resources">
+              <li>fetch API</li>
+              <li>
+                <a href="https://superheroapi.com/">Superhero API</a>
+              </li>
+            </ul>
+          </div>
+        </div>
         <ProjectSuperhero closeMe={this.state.close} />
         <Close className={"close-btn " + (this.state.close ? "hide" : "")} onClick={this.closeProject.bind(this)} />
       </div>
